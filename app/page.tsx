bfac2a1839d8734ef5828e85534eaf50f148f4a8
siyeol97 <leesi2830@naver.com>
2024-02-13 16:50:32 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from '../components/Navigation';
+
+export default function Home() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Hello Next.js 14!</h1>
+    </div>
+  );
+}
